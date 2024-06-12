21f5d0bdc350a5b800f945cb7b61476736bf3fa4
Jesse <sora4222@gmail.com>
2024-06-12 17:19:54 +1000

    Adds the keyboard press events

--- main.go
+++ main.go
@@ -1,67 +1,128 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"gioui.org/app"
+	"gioui.org/io/event"
+	"gioui.org/io/key"
 	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
 	"gioui.org/widget"
-	"gioui.org/widget/material"
 	"image"
-	"image/draw"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
+type ProcArgs struct {
+	labels    map[rune]string
+	imgDir    string
+	keyFilter []event.Filter
+}
+
+func processArgs() (*ProcArgs, error) {
+	args := os.Args[1:]
+	pathInfo, err := os.Stat(args[0])
+	if err != nil || pathInfo.IsDir() != true {
+		return nil, err
+	}
+	labelsMap := make(map[rune]string)
+	filter := make([]event.Filter, 0, len(args[1:]))
+	for _, val := range args[1:] {
+		labelCharSplit := strings.Split(val, "=")
+		if len(labelCharSplit) != 2 {
+			return nil, errors.New(fmt.Sprintf("label and key should be separated by a single '=', %v", val))
+		}
+		// Need to add validation for label for directory
+		// Check character hasn't been added before
+		char := []rune(labelCharSplit[1])[0]
+		if _, ok := labelsMap[char]; ok {
+			return nil, errors.New(fmt.Sprintf("keyboard character has been assigned twice %v", char))
+		}
+		labelsMap[char] = labelCharSplit[0]
+		filter = append(filter, key.Filter{Name: key.Name(unicode.ToUpper(char))})
+	}
+	filter = append(filter, key.Filter{Name: key.NameBack})
+	return &ProcArgs{imgDir: args[0], labels: labelsMap, keyFilter: filter}, nil
+}
+
 func main() {
+	procArgs, err := processArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
 	go func() {
 		window := new(app.Window)
-		imageDir := ""
-		files, err := os.ReadDir(imageDir)
+
+		err := draw(window, procArgs)
 		if err != nil {
-			os.Exit(1)
-		}
-		for _, imagePath := range files {
-			if imagePath.IsDir() {
-				continue
-			}
-			err := run(window, imageDir+"/"+imagePath.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
+			log.Fatal(err)
 		}
 		os.Exit(0)
 	}()
 	app.Main()
 }
 
-func run(window *app.Window, imagePath string) error {
-	theme := material.NewTheme()
+func draw(window *app.Window, args *ProcArgs) error {
+	//theme := material.NewTheme()
 	var ops op.Ops
-	imgFile, err := os.Open(imagePath)
-	if err != nil {
-		return err
-	}
-	img, _, err := image.Decode(imgFile)
-	err = imgFile.Close()
-	if err != nil {
-		return err
-	}
-	imgRGBA, ok := img.(*image.RGBA)
-	if !ok {
-		b := img.Bounds()
-		imgRGBA = image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-		draw.Draw(imgRGBA, imgRGBA.Bounds(), img, b.Min, draw.Src)
-	}
+	imagesToView, _ := os.ReadDir(args.imgDir)
+	currentImgIndex := 0
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
 			return e.Err
 		case app.FrameEvent:
+			if imagesToView[currentImgIndex].IsDir() {
+				currentImgIndex++
+				continue
+			}
+
 			// This graphics context is used for managing the rendering state.
 			gtx := app.NewContext(&ops, e)
-			// Define a large label with an appropriate text
-			title := material.H1(theme, "Label to place the image")
-			widget.Image{}
-			title.Layout(gtx)
+			// register a global key listener for the escape key wrapping our entire UI.
+			area := clip.Rect{Max: gtx.Constraints.Max}.Push(gtx.Ops)
+			event.Op(gtx.Ops, "tagVal")
+			for {
+				ev, ok := gtx.Event(args.keyFilter...)
+				if !ok {
+					return errors.New("KeyFilter error")
+				}
+
+				switch ev := ev.(type) {
+				case key.Event:
+					switch ev.Name {
+					case key.NameBack:
+						// TODO: Implement backspace
+						fmt.Println("Pressed backspace")
+					default:
+						// Copy the file to the other locations
+						fmt.Println("Pressed a labelling key")
+						currentImgIndex++
+					}
+				}
+
+				// Process the image
+				imgFile, err := os.Open(args.imgDir + "/" + imagesToView[currentImgIndex].Name())
+				if err != nil {
+					return err
+				}
+				window.Option(app.Title(imgFile.Name()))
+
+				img, _, err := image.Decode(imgFile)
+				err = imgFile.Close()
+				if err != nil {
+					return err
+				}
+				imgOp := paint.NewImageOp(img)
+				widget.Image{Src: imgOp, Fit: widget.ScaleDown}.Layout(gtx)
+
+				area.Pop()
+				e.Frame(gtx.Ops)
+			}
 		}
 	}
 }
